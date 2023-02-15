d271f15f6b214b338d21a06c5ece8e4bc8b3d7e9
2023LEAP3-Turuu <turuu@NEST-96s-iMac.local>
2023-02-15 12:13:16 +0800

    test

--- src/components/ProductsCard.tsx
+++ src/components/ProductsCard.tsx
@@ -1,10 +1,12 @@
 import { Text, View } from "react-native";
 import { SvgXml } from "react-native-svg";
 
-export const ProductsCard = ({ lists }) => (
-  <View>
-    <SvgXml className="w-[160px] h-[185px] rounded" xml={lists.xml} />
-    <Text className="text-[#2D2A2B] text-[16px] leading-[20px] font-medium pt-[10] pb-[2]">{lists.name}</Text>
-    <Text className="text-[#2D2A2B] text-[13px] leading-[20px] font-light ">{lists.price}</Text>
-  </View>
-);
+export const ProductsCard = ({ lists }) => {
+  return (
+    <View>
+      <SvgXml className="w-[160px] h-[185px] rounded" xml={lists.xml} />
+      <Text className="text-[#2D2A2B] text-[16px] leading-[20px] font-medium pt-[10] pb-[2]">{lists.name}</Text>
+      <Text className="text-[#2D2A2B] text-[13px] leading-[20px] font-light ">{lists.price}</Text>
+    </View>
+  );
+};
